24c0a0cba2a0fc154d8bb8d625e69fcb738810be
IsraPerez98 <isra1998isra@gmail.com>
2022-10-03 21:45:20 -0300

    ✨ Mejoras al diseño de la consola

--- src/components/Grafo/Consola/Consola.svelte
+++ src/components/Grafo/Consola/Consola.svelte
@@ -23,19 +23,27 @@
 
 {#if textoExplicativo && textoExplicativo.length > 0}
     <div class="absolute bottom-0 right-0">
-        <div class="flex items-center justify-between rounded-t-lg bg-indigo-700 py-4 px-9">
-            <p class="text-lg">
-                Información del Algoritmo
+        <div class="flex items-center justify-between rounded-t-lg bg-indigo-700 pl-16 py-2 px-9">
+            <p class="text-base">
+                Información del Algoritmo de Flujo Máximo
             </p>
         </div>
-        <div bind:this={divTextoExplicativo} class="bg-white overflow-auto h-72 shadow-lg ring-1 flex flex-col divide-y ">
-            {#each textoExplicativo as linea}
-                <div class="min-h-12 py-3 mx-4 flex items-center max-w-96 w-96 text-clip">
-                    <p class="text-slate-900 text-sm font-medium">
-                        {linea}
-                    </p>
-                </div>
-            {/each}
+        <div class="flex flex-row">
+            <div class="flex flex-col h-auto py-2 px-1 w-12 bg-slate-900 text-xs text-center ">
+                <!--TODO: MENU PARA CATEGORIAS-->
+                <button class="py-3 hover:bg-gray-700 active:bg-gray-700 ">
+                    Info
+                </button>
+            </div>
+            <div bind:this={divTextoExplicativo} class="bg-white overflow-auto h-72 shadow-lg ring-1 flex flex-col divide-y ">
+                {#each textoExplicativo as linea}
+                    <div class="min-h-12 py-3 mx-4 flex items-center max-w-96 w-96 text-clip">
+                        <p class="text-slate-900 text-sm font-medium">
+                            {linea}
+                        </p>
+                    </div>
+                {/each}
+            </div>
         </div>
     </div>
 {/if}
