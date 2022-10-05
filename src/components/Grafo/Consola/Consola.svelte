661f7a6b48c11ceb279ddb665a3ff262e9385fc5
IsraPerez98 <isra1998isra@gmail.com>
2022-10-04 21:34:20 -0300

    📦️ Texto explicativo en componente aparte

--- src/components/Grafo/Consola/Consola.svelte
+++ src/components/Grafo/Consola/Consola.svelte
@@ -1,24 +1,12 @@
 <script lang="ts">
-    import { afterUpdate, beforeUpdate } from "svelte";
     import type Consola from "../../../classes/Consola";
+
+    import Explicacion from "./Explicacion.svelte";
 
     export let consola: Consola;
 
     $: textoExplicativo = consola.textoExplicativo;
 
-    let divTextoExplicativo = null;
-
-    
-
-    function autoScroll(): void {
-        if(divTextoExplicativo) {
-            console.log("autoscroll");
-            console.log(divTextoExplicativo.scrollHeight);
-            divTextoExplicativo.scrollTop = divTextoExplicativo.scrollHeight;
-        }
-    }
-
-    afterUpdate(autoScroll);
 </script>
 
 {#if textoExplicativo && textoExplicativo.length > 0}
@@ -35,15 +23,9 @@
                     Info
                 </button>
             </div>
-            <div bind:this={divTextoExplicativo} class="bg-white overflow-auto h-72 shadow-lg ring-1 flex flex-col divide-y ">
-                {#each textoExplicativo as linea}
-                    <div class="min-h-12 py-3 mx-4 flex items-center max-w-96 w-96 text-clip">
-                        <p class="text-slate-900 text-sm font-medium">
-                            {linea}
-                        </p>
-                    </div>
-                {/each}
-            </div>
+            <Explicacion
+                textoExplicativo={textoExplicativo}
+            />
         </div>
     </div>
 {/if}
---
+++ src/components/Grafo/Consola/Explicacion.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { afterUpdate } from "svelte";
+    export let textoExplicativo: string[];
+
+    let divTextoExplicativo: HTMLDivElement;
+
+    function autoScroll(): void {
+        if(divTextoExplicativo) {
+            console.log("autoscroll");
+            console.log(divTextoExplicativo.scrollHeight);
+            divTextoExplicativo.scrollTop = divTextoExplicativo.scrollHeight;
+        }
+    }
+
+    afterUpdate(autoScroll);
+</script>
+
+<div bind:this={divTextoExplicativo} class="bg-white overflow-auto h-72 shadow-lg ring-1 flex flex-col divide-y ">
+    {#each textoExplicativo as linea}
+        <div class="min-h-12 py-3 mx-4 flex items-center max-w-96 w-96 text-clip">
+            <p class="text-slate-900 text-sm font-medium">
+                {linea}
+            </p>
+        </div>
+    {/each}
+</div>
