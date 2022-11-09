354298e33c0259f873f1625548589d3874f50478
IsraPerez98 <isra1998isra@gmail.com>
2022-11-08 22:10:58 -0300

    Updates

--- src/interfaces/MatrizAdyacencia.ts
+++
@@ -1,5 +0,0 @@
-interface MatrizAdyacencia extends Array<Array<number>> { } //arreglo blidimensional con los pesos de las aristas
-
-
-
-export default MatrizAdyacencia;
--- src/interfaces/Posicion.ts
+++
@@ -1,6 +0,0 @@
-interface Posicion {
-    x: number; 
-    y: number; 
-}
-
-export default Posicion;
