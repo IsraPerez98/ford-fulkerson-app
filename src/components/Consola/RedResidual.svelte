54b4a009cebe523f495536d1b8829c0808c65687
IsraPerez98 <isra1998isra@gmail.com>
2022-11-18 22:06:50 -0300

    🪲 Bugfix scroll red residual

--- src/components/Consola/RedResidual.svelte
+++ src/components/Consola/RedResidual.svelte
@@ -6,6 +6,9 @@
     $: dimension = redResidual.length;
     $: matrizColores = matrizAdyacencia.map((fila, i) => fila.map((_, j) => {
         if (matrizAdyacencia[i][j] !== redResidual[i][j]) {
+            if(redResidual[i][j] === 0) {
+                return "bg-rose-700";
+            }
             return "bg-rose-400";
         } else if (redResidual[i][j] <= 0) {
             return "bg-gray-200";
@@ -16,30 +19,30 @@
 
 </script>
 
-<div class="overflow-auto text-white text-sm font-medium text-center p-4">
-    <div style="grid-template-columns: repeat({dimension + 1}, minmax(0, 1fr));" class="grid gap-3">
+<div class="overflow-auto text-white text-sm font-medium text-center p-2">
+    <div style="grid-template-columns: repeat({dimension + 1}, minmax(max-content, auto));" class="items-center grid auto-rows-max auto-cols-max gap-3">
         {#each Array(dimension + 1) as _, i}
             {#each Array(dimension + 1) as _, j}
                 {#if i === 0 && j === 0}
-                    <div class="p-2 rounded-lg shadow-lg bg-slate-800">
+                    <div class="py-2 rounded-lg shadow-lg bg-slate-800 w-14">
                         <p >
                             Índice
                         </p>
                     </div>
                 {:else if i === 0}
-                    <div class="p-2 rounded-lg shadow-lg bg-slate-800">
+                    <div class="py-2 rounded-lg shadow-lg bg-slate-800 w-14">
                         <p >
                             {j-1}
                         </p>
                     </div>
                 {:else if j === 0}
-                    <div class="p-2 rounded-lg shadow-lg bg-slate-800">
+                    <div class="py-2 rounded-lg shadow-lg bg-slate-800 w-14 ">
                         <p >
                             {i-1}
                         </p>
                     </div>
                 {:else}
-                    <div class="p-2 rounded-lg shadow-lg text-slate-800 {matrizColores[i-1][j-1]}">
+                    <div class="py-2 rounded-lg shadow-lg text-slate-800 w-14 {matrizColores[i-1][j-1]}">
                         <p>
                             {redResidual[i - 1][j - 1]}
                         </p>
