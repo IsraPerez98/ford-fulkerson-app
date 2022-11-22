925cc15bdd68279d59418edad901c329de8af461
IsraPerez98 <isra1998isra@gmail.com>
2022-11-22 18:31:13 -0300

    ⚠️ Iniciar algoritmo antes de ver explicacion

--- src/components/Consola/Explicacion.svelte
+++ src/components/Consola/Explicacion.svelte
@@ -25,14 +25,23 @@
 
 </script>
 
-<div bind:this={divScroll} class="overflow-auto ">
-    {#each textoExplicativo as linea}
+<div bind:this={divScroll} class="overflow-auto h-full w-full">
+    {#if textoExplicativo.length === 0}
+        <div class="text-center text-gray-500 h-full w-full flex">
+            <p class="text-xl m-auto">
+                Se debe iniciar el algoritmo para ver su explicación.
+            </p>
+        </div>
+    {:else}
+        {#each textoExplicativo as linea}
             <div class="min-h-12 py-3 mx-4 flex items-center text-clip">
                 <p use:twemoji={{className: 'emoji-explicacion'}} class="flex flex-row text-slate-900 text-sm font-medium">
                     {linea}
                 </p>
             </div>
-    {/each}
+        {/each}
+    {/if}
+    
 </div>
 
 <style global>
