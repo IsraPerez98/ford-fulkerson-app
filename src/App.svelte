19c95a93492a1b622eadfdebda6574957294a6bf
IsraPerez98 <isra1998isra@gmail.com>
2022-11-28 04:12:33 -0300

    ⚠️ Mostrar aviso responsive

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-
 	import Modal from 'svelte-simple-modal';
 
 	import Tailwindcss from './Tailwindcss.svelte';
 	import Canvas from './components/Canvas/Canvas.svelte';
+
+	import mobile from 'is-mobile';
 	
 	$: innerWidth = window.innerWidth;
 	$: innerHeight = window.innerHeight;
+
+	if (mobile()) {
+		window.alert("Este sitio no está optimizado para dispositivos móviles. Se recomienda acceder desde un dispositivo de escritorio.");
+	}
 
 </script>
 
